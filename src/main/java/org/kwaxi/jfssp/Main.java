99b241480e2e5ad74f1f7f21e807090cc3edea76
Daniel Wagner <daniel@wagners.name>
2008-05-31 12:34:16 +0000

    .

--- src/main/java/org/kwaxi/jfssp/Main.java
+++ src/main/java/org/kwaxi/jfssp/Main.java
@@ -1,33 +1,40 @@
 /*
- *  JFSSP - A Flow Shop Scheduler in Java
- *  
+ *  FSSP - A Flow Shop Scheduler in Java
+ *
  *  Copyright (C) 2007  Daniel Wagner <dwkwaxi@gmail.com>
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ *
  *  $Id$
  */
 
 package org.kwaxi.jfssp;
 
 import org.apache.commons.cli.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author Daniel Wagner <dwkwaxi@gmail.com>
- * 
+ *
  */
 public class Main {
+
+	/**
+	 * My Log class.
+	 */
+	private static Log log = LogFactory.getLog(Main.class);
 
 	/**
 	 * @param args
@@ -41,27 +48,27 @@ public class Main {
 
 		options.addOption(OptionBuilder.hasArg().withArgName("gn").withLongOpt(
 				"generations").withDescription(
-				"Number of generations [default: 50]").withType(new Integer(0))
-				.create("g"));
+				"Number of generations [default: 50]").withType(
+				Integer.valueOf(0)).create("g"));
 
 		options.addOption(OptionBuilder.hasArg().withArgName("mp").withLongOpt(
 				"mutation").withDescription(
-				"Mutation propability [default: 0.5]").withType(new Double(0))
-				.create("m"));
+				"Mutation propability [default: 0.5]").withType(
+				Double.valueOf(0)).create("m"));
 
 		options.addOption(OptionBuilder.hasArg().withArgName("ps").withLongOpt(
 				"populationsize").withDescription(
-				"Size of population [default: 20]").withType(new Integer(0))
-				.create("p"));
+				"Size of population [default: 20]")
+				.withType(Integer.valueOf(0)).create("p"));
 
 		options.addOption(OptionBuilder.hasArg().withArgName("rp").withLongOpt(
 				"recombination").withDescription(
 				"Recombination propability [default: 0.8]").withType(
-				new Double(0)).create("r"));
+				Double.valueOf(0)).create("r"));
 
 		options.addOption(OptionBuilder.hasArg().withArgName("sp").withLongOpt(
 				"selectionpressure").withDescription(
-				"Selection pressure [default: 4]").withType(new Integer(0))
+				"Selection pressure [default: 4]").withType(Integer.valueOf(0))
 				.create("s"));
 
 		options.addOption(OptionBuilder.withLongOpt("help").withDescription(
@@ -69,8 +76,8 @@ public class Main {
 
 		options.addOption(OptionBuilder.hasArg().withArgName("filename")
 				.isRequired().withLongOpt("file").withDescription(
-						"Problem file [default: \"\"]").withType(new String())
-				.create("f"));
+						"Problem file [default: \"\"]").withType(
+						String.valueOf("")).create("f"));
 
 		options.addOptionGroup(new OptionGroup().addOption(
 				OptionBuilder.withLongOpt("verbose").withDescription(
@@ -89,35 +96,39 @@ public class Main {
 			if (line.hasOption("h")) {
 				// automatically generate the help statement
 				HelpFormatter formatter = new HelpFormatter();
-				formatter.printHelp("jfssp", options);
+				formatter.printHelp("fssp", options);
 			}
-			
-		}
-		catch (MissingOptionException exp ){
-			System.out.println("An option was missing");
-			
-			// automatically generate the help statement
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("jfssp", options);
-		}
-		catch (MissingArgumentException exp ){
-			System.out.println("An argument was missing");
+
+		} catch (MissingOptionException exp) {
+			log.info("An option was missing:" + exp.getMessage(), exp);
 
 			// automatically generate the help statement
 			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("jfssp", options);
-		}
-		catch (AlreadySelectedException exp ){
-			// automatically generate the help statement
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("jfssp", options);
-		}
-		catch (ParseException exp) {
-			System.out.println("Unexpected exception:" + exp.getMessage());
+			formatter.printHelp("fssp", options);
+		} catch (MissingArgumentException exp) {
+			log.info("An argument was missing:" + exp.getMessage(), exp);
 
 			// automatically generate the help statement
 			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("jfssp", options);
+			formatter.printHelp("fssp", options);
+		} catch (AlreadySelectedException exp) {
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
+		} catch (ParseException exp) {
+			log.info("Unexpected exception:" + exp.getMessage(), exp);
+
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
+
+			System.exit(1);
 		}
+
+		// Ausgabe der eingestellten Optionen
+
+		log.info("Configuration");
+		// log.info(" Datafile: " + fname);
 	}
+
 }
