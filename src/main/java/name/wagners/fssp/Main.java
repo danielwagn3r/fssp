b7f3b25f58917e89c597b5a6edf7f6198e53adaf
Daniel Wagner <daniel@wagners.name>
2013-07-29 12:41:54 +0000

    use @Slf4j

--- src/main/java/name/wagners/fssp/Main.java
+++ src/main/java/name/wagners/fssp/Main.java
@@ -21,6 +21,8 @@
 
 package name.wagners.fssp;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.apache.commons.cli.AlreadySelectedException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -32,113 +34,93 @@ import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * @author Daniel Wagner <daniel@wagners.name>
  *
  */
+@Slf4j
 public class Main {
 
-    /**
-     * My Log class.
-     */
-    private static Log log = LogFactory.getLog(Main.class);
+	/**
+	 * @param args
+	 *            command-line arguments
+	 */
+	public static void main(final String[] args) {
+		// create the command line parser
+		CommandLineParser parser = new PosixParser();
 
-    /**
-     * @param args command-line arguments
-     */
-    public static void main(final String[] args) {
-        // create the command line parser
-        CommandLineParser parser = new PosixParser();
+		// create the Options
+		Options options = new Options();
 
-        // create the Options
-        Options options = new Options();
+		options.addOption(OptionBuilder.hasArg().withArgName("gn").withLongOpt("generations")
+				.withDescription("Number of generations [default: 50]").withType(Integer.valueOf(0)).create("g"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("gn").withLongOpt(
-                "generations").withDescription(
-                "Number of generations [default: 50]").withType(
-                Integer.valueOf(0)).create("g"));
+		options.addOption(OptionBuilder.hasArg().withArgName("mp").withLongOpt("mutation")
+				.withDescription("Mutation propability [default: 0.5]").withType(Double.valueOf(0)).create("m"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("mp").withLongOpt(
-                "mutation").withDescription(
-                "Mutation propability [default: 0.5]").withType(
-                Double.valueOf(0)).create("m"));
+		options.addOption(OptionBuilder.hasArg().withArgName("ps").withLongOpt("populationsize")
+				.withDescription("Size of population [default: 20]").withType(Integer.valueOf(0)).create("p"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("ps").withLongOpt(
-                "populationsize").withDescription(
-                "Size of population [default: 20]")
-                .withType(Integer.valueOf(0)).create("p"));
+		options.addOption(OptionBuilder.hasArg().withArgName("rp").withLongOpt("recombination")
+				.withDescription("Recombination propability [default: 0.8]").withType(Double.valueOf(0)).create("r"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("rp").withLongOpt(
-                "recombination").withDescription(
-                "Recombination propability [default: 0.8]").withType(
-                Double.valueOf(0)).create("r"));
+		options.addOption(OptionBuilder.hasArg().withArgName("sp").withLongOpt("selectionpressure")
+				.withDescription("Selection pressure [default: 4]").withType(Integer.valueOf(0)).create("s"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("sp").withLongOpt(
-                "selectionpressure").withDescription(
-                "Selection pressure [default: 4]").withType(Integer.valueOf(0))
-                .create("s"));
+		options.addOption(OptionBuilder.withLongOpt("help").withDescription("print this message").create("h"));
 
-        options.addOption(OptionBuilder.withLongOpt("help").withDescription(
-                "print this message").create("h"));
+		options.addOption(OptionBuilder.hasArg().withArgName("filename").isRequired().withLongOpt("file")
+				.withDescription("Problem file [default: \"\"]").withType(String.valueOf("")).create("f"));
 
-        options.addOption(OptionBuilder.hasArg().withArgName("filename")
-                .isRequired().withLongOpt("file").withDescription(
-                        "Problem file [default: \"\"]").withType(
-                        String.valueOf("")).create("f"));
+		options.addOptionGroup(new OptionGroup().addOption(
+				OptionBuilder.withLongOpt("verbose").withDescription("be extra verbose").create("v")).addOption(
+				OptionBuilder.withLongOpt("quiet").withDescription("be extra quiet").create("q")));
 
-        options.addOptionGroup(new OptionGroup().addOption(
-                OptionBuilder.withLongOpt("verbose").withDescription(
-                        "be extra verbose").create("v")).addOption(
-                OptionBuilder.withLongOpt("quiet").withDescription(
-                        "be extra quiet").create("q")));
+		options.addOption(OptionBuilder.withLongOpt("version")
+				.withDescription("print the version information and exit").create("V"));
 
-        options.addOption(OptionBuilder.withLongOpt("version").withDescription(
-                "print the version information and exit").create("V"));
+		try {
+			// parse the command line arguments
+			CommandLine line = parser.parse(options, args);
 
-        try {
-            // parse the command line arguments
-            CommandLine line = parser.parse(options, args);
+			// validate that block-size has been set
+			if (line.hasOption("h")) {
+				// automatically generate the help statement
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("fssp", options);
+			}
 
-            // validate that block-size has been set
-            if (line.hasOption("h")) {
-                // automatically generate the help statement
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("fssp", options);
-            }
+		} catch (MissingOptionException exp) {
+			log.info("An option was missing:" + exp.getMessage(), exp);
 
-        } catch (MissingOptionException exp) {
-            log.info("An option was missing:" + exp.getMessage(), exp);
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
+		} catch (MissingArgumentException exp) {
+			log.info("An argument was missing:" + exp.getMessage(), exp);
 
-            // automatically generate the help statement
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("fssp", options);
-        } catch (MissingArgumentException exp) {
-            log.info("An argument was missing:" + exp.getMessage(), exp);
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
+		} catch (AlreadySelectedException exp) {
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
+		} catch (ParseException exp) {
+			log.info("Unexpected exception:" + exp.getMessage(), exp);
 
-            // automatically generate the help statement
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("fssp", options);
-        } catch (AlreadySelectedException exp) {
-            // automatically generate the help statement
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("fssp", options);
-        } catch (ParseException exp) {
-            log.info("Unexpected exception:" + exp.getMessage(), exp);
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("fssp", options);
 
-            // automatically generate the help statement
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("fssp", options);
+			System.exit(1);
+		}
 
-            System.exit(1);
-        }
+		// Ausgabe der eingestellten Optionen
 
-        // Ausgabe der eingestellten Optionen
-
-        log.info("Configuration");
-        // log.info(" Datafile: " + fname);
-    }
+		log.info("Configuration");
+		// log.info(" Datafile: {}", fname);
+	}
 
 }
