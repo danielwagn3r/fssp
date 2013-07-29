609319b6ec54d70a793c49fccff52d30efd16868
Daniel Wagner <daniel@wagners.name>
2013-07-29 11:47:00 +0000

    use SLF4j instead of commons-logging use Lombok updated to JUnit 4.11

--- src/main/java/name/wagners/fssp/instance/package-info.java
+++ src/main/java/name/wagners/fssp/instance/package-info.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * $Id: Main.java 1234 2011-10-06 13:49:28Z WAGNERDA $
+ * $Id$
  */
 
 /**
